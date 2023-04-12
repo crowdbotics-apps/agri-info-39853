6196ff5042318437a379e88809a52ceb7026e3bf
Crowdbotics <team@crowdbotics.com>
2023-04-12 05:21:01 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/agri_info_39853/MainActivity.java
@@ -0,0 +1,15 @@
+package com.agri_info_39853;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "agri_info_39853";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "agri-info-39853",
+  url: "https://agri-info-39853.botics.co",
+  api: "https://agri-info-39853.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
